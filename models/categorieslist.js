e91516989e578536cef4d5fa917fcc40fab5587b
prabingeorge <prabingeorge@gmail.com>
2026-01-07 23:04:36 +0530

    Added code

--- models/categorieslist.js
+++ models/categorieslist.js
@@ -32,29 +32,3 @@ export default (sequelize, DataTypes) => {
   });
   return CategoriesLists;
 };
-
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class CategoriesList extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   }
-//   CategoriesList.init({
-//     type: DataTypes.STRING,
-//     image_name: DataTypes.STRING,
-//     category_id: DataTypes.INTEGER
-//   }, {
-//     sequelize,
-//     modelName: 'CategoriesList',
-//   });
-//   return CategoriesList;
-// };
